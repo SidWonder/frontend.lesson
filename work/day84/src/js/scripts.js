81a867e8b824749809e7522c36021dade347c45b
Andrey <andrey-trush@mail.ru>
2020-04-05 16:44:05 +0300

    Prengi - Верстка 2 секции

---
+++ work/day84/src/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $('.promo-slider').slick({
+    arrows: true,
+    dots: true,
+    //infinite: true,
+    speed: 1200,
+    fade: true,
+    cssEase: 'ease-out',
+    dotsClass: "my-dots",
+    prevArrow: '<button type="button" class="review-slider-prev"></button>',
+    nextArrow: '<button type="button" class="review-slider-next"></button>'
+  });
+});
